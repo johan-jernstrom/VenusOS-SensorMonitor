d93afba325c020acea41fb40a35b7441cdb430d7
johan-jernstrom <johan@nagu.se>
2025-06-16 23:57:38 +0200

    log in csv logger and inlcude milliseconds in timestamp

--- CSVLogger.py
+++ CSVLogger.py
@@ -2,14 +2,19 @@ import os
 import csv
 import time
 from datetime import datetime
+import logging
 
 class CSVLogger:
     def __init__(self, directory, flush_interval=30):
+        self.logger = logging.getLogger(__name__)
+        self.logger.info("Initializing CSVLogger")
         if not os.path.isabs(directory):
             directory = os.path.abspath(directory)
         if not os.path.exists(directory):
             os.makedirs(directory)
+            self.logger.info(f"Created directory: {directory}")
         self.directory = directory
+        self.logger.info(f"Using directory: {self.directory}")
         self.buffer = []
         self.flush_interval = flush_interval
         self.last_flush_time = time.time()
@@ -38,7 +43,7 @@ class CSVLogger:
             b2_voltage, b2_current, b2_smoothed, 
             b3_voltage, b3_current, b3_smoothed):
         self.buffer.append([
-            datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+            datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f"),
             round(current, 2),
             round(voltage, 2),
             round(b1_voltage, 2),
@@ -56,6 +61,7 @@ class CSVLogger:
             self.flush()
 
     def flush(self):
+        self.logger.debug("Flushing buffer to CSV file")
         if not self.buffer:
             return
         current_date = datetime.now().strftime("%Y%m%d")
